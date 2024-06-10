ddb0eebbba45c90145081b6fcef3ca82de25d1f5
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 14:51:55 +0800

    Update reddit_cross.sh

--- scripts/reddit_cross.sh
+++ scripts/reddit_cross.sh
@@ -1,13 +1,8 @@
 # synthetic graph
 for seed in 1 2 3 4 5
-    do
+do
     for model in 'GCN' 'SGC' 'SAGE' 'GIN' 'JKNet' 'MLP' 
     do
-        python -u SimGC_inductive.py --dataset reddit --reduction_rate=0.002 --teacher_model=SGC --model=${model} --gpu_id=1 --seed=${seed} 
-    done
-
-    for model in 'GCN' 'SGC' 'SAGE' 'GIN' 'JKNet' 'MLP' 
-    do
-        python -u SimGC_inductive.py --dataset reddit --reduction_rate=0.002 --teacher_model=GCN --model=${model} --gpu_id=1 --seed=${seed} 
+        python -u SimGC_inductive.py --dataset reddit --reduction_rate=0.002  --lr_adj=0.01 --lr_feat=0.05 --feat_alpha=100 --smoothness_alpha=0.1 --condensing_loop=2500 --teacher_model=SGC --model=${model} --gpu_id=1 --seed=${seed} 
     done
 done
