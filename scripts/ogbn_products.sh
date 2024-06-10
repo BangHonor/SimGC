5beb2321b6a5637372fb8f17ea7699ce5cde0f0e
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 14:48:44 +0800

    Update ogbn_products.sh

--- scripts/ogbn_products.sh
+++ scripts/ogbn_products.sh
@@ -7,6 +7,6 @@ for seed in 1 2 3 4 5
 do
     for reduction_rate in 0.0025 0.005 0.01
     do
-        python -u SimGC.py --dataset ogbn-products --reduction_rate=${reduction_rate} --threshold=0.05  --seed=${seed} --teacher_model='SGC' --validation_model='GCN' 
+        python -u SimGC.py --dataset ogbn-products --reduction_rate=${reduction_rate}  --lr_adj=0.01 --lr_feat=0.05 --feat_alpha=10 --smoothness_alpha=0.1 --condensing_loop=1500 --teacher_model=SGC_Multi  --seed=${seed} --validation_model='GCN' 
     done
 done
