3da0609fa22bedd171de445536ddf3508cda4494
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 14:42:31 +0800

    Update cora_cross.sh

--- scripts/cora_cross.sh
+++ scripts/cora_cross.sh
@@ -3,6 +3,6 @@ for seed in 1 2 3 4 5
     do
     for model in 'GCN' 'SGC' 'SAGE' 'GIN' 'JKNet' 'MLP' 
     do
-        python -u SimGC.py --dataset cora --lr_adj=0.001 --lr_feat=0.05 --reduction_rate=0.013 --teacher_model=SGC --model=${model} --gpu_id=1 --seed=${seed}
+        python -u SimGC.py --dataset cora --lr_adj=0.001 --lr_feat=0.005 --reduction_rate=0.5 --condensing_loop=1500 --teacher_model=SGC_Multi  --model=${model} --gpu_id=1 --seed=${seed}
     done
 done
