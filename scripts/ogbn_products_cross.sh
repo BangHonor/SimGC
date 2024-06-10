c1828d7d7272561cfb91c0b09b9833e706ab602e
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 14:49:13 +0800

    Update ogbn_products_cross.sh

--- scripts/ogbn_products_cross.sh
+++ scripts/ogbn_products_cross.sh
@@ -3,6 +3,6 @@ for reduction_rate in 0.005
     do
     for model in 'GCN' 'SGC' 'SAGE' 'GIN' 'JKNet' 'MLP' 
     do
-        python -u SimGC.py --dataset ogbn-products --reduction_rate=${reduction_rate} --teacher_model=GCN --model=${model} --seed=1
+        python -u SimGC.py --dataset ogbn-products --reduction_rate=${reduction_rate}  --lr_adj=0.01 --lr_feat=0.05 --feat_alpha=10 --smoothness_alpha=0.1 --condensing_loop=3500 --teacher_model=SGC_Multi --model=${model} --seed=1
     done
 done
