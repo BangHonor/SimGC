5ec05fd581ebbe82bec4c4a6858451f1d97e878c
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 14:43:01 +0800

    Update ogbn_arxiv.sh

--- scripts/ogbn_arxiv.sh
+++ scripts/ogbn_arxiv.sh
@@ -2,6 +2,6 @@ for seed in 1 2 3 4 5
 do
     for reduction_rate in 0.001 0.005 0.01
     do
-        python -u SimGC.py --dataset ogbn-arxiv --reduction_rate=${reduction_rate} --teacher_model=SGC --gpu_id=1 --seed=${seed}
+        python -u SimGC.py --dataset ogbn-arxiv --reduction_rate=${reduction_rate} --teacher_model=SGC --condensing_loop=1500 --gpu_id=1 --seed=${seed}
     done
 done
