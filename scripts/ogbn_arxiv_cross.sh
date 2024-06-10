1a04b0e7dd27cd17f5635ec49a9650cd1bbd4cd4
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 15:13:39 +0800

    Update ogbn_arxiv_cross.sh

--- scripts/ogbn_arxiv_cross.sh
+++ scripts/ogbn_arxiv_cross.sh
@@ -3,6 +3,6 @@ for seed in 1 2 3 4 5
     do
     for model in 'GCN' 'SGC' 'SAGE' 'GIN' 'JKNet' 'MLP' 
     do
-        python -u SimGC.py --dataset ogbn-arxiv --reduction_rate=0.01  --lr_adj=0.01 --lr_feat=0.05 --feat_alpha=10 --smoothness_alpha=0.1 --condensing_loop=1500 --teacher_model=SGC_Multi --model=${model} --seed=${seed} 
+        python -u SimGC.py --dataset ogbn-arxiv --reduction_rate=0.01  --lr_adj=0.01 --lr_feat=0.05 --feat_alpha=5 --smoothness_alpha=0.1 --condensing_loop=1500 --teacher_model=SGC --model=${model} --seed=${seed} 
     done
 done
