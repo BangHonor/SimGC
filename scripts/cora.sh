ab3a16e1f47706bf2b28d03668ba427a05413352
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 14:41:53 +0800

    Update cora.sh

--- scripts/cora.sh
+++ scripts/cora.sh
@@ -2,6 +2,6 @@ for seed in 1 2 3 4 5
 do
     for reduction_rate in 0.25 0.5 1
     do
-        python -u SimGC.py --dataset cora --lr_adj=0.001 --lr_feat=0.05 --reduction_rate=${reduction_rate} --teacher_model=SGC --gpu_id=0 --seed=${seed}
+        python -u SimGC.py --dataset cora --lr_adj=0.001 --lr_feat=0.005 --reduction_rate=${reduction_rate} --condensing_loop=1500 --teacher_model=SGC_Multi --gpu_id=0 --seed=${seed}
     done
 done
