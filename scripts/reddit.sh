8fed1eb8c03b8f1254453fe1b767271d8416f3bd
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 14:50:40 +0800

    Update reddit.sh

--- scripts/reddit.sh
+++ scripts/reddit.sh
@@ -1,12 +1,8 @@
-for reduction_rate in 0.0005 0.001
-do
-    python -u SimGC_inductive.py --dataset reddit --reduction_rate=${reduction_rate} --teacher_model=SGC --model=GCN --gpu_id=1 --seed=1 
-done
 
-for seed in 2 3 4 5
+for seed in 1 2 3 4 5
 do
     for reduction_rate in 0.0005 0.001 0.002
     do
-        python -u SimGC_inductive.py --dataset reddit --reduction_rate=${reduction_rate} --teacher_model=SGC --model=GCN --gpu_id=1 --seed=${seed}
+        python -u SimGC_inductive.py --dataset reddit --reduction_rate=${reduction_rate}  --lr_adj=0.01 --lr_feat=0.05 --feat_alpha=100 --smoothness_alpha=0.1 --condensing_loop=2500 --teacher_model=SGC --model=GCN --gpu_id=1 --seed=${seed}
     done
 done
