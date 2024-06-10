83dbf84900b120ccc977e777a9b74560390409ad
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2024-06-10 14:58:08 +0800

    Update flickr.sh

--- scripts/flickr.sh
+++ scripts/flickr.sh
@@ -2,6 +2,6 @@ for seed in 1 2 3 4 5
 do
     for reduction_rate in 0.001 0.005 0.01
     do
-        python -u SimGC_induct.py --dataset flickr --reduction_rate=${reduction_rate} --teacher_model=SGC --gpu_id=1 --seed=${seed}
+        python -u SimGC_induct.py --dataset flickr --reduction_rate=${reduction_rate}  --lr_adj=0.01 --lr_feat=0.05 --feat_alpha=5 --smoothness_alpha=0.1 --condensing_loop=3500 --teacher_model=SGC --gpu_id=1 --seed=${seed}
     done
 done
